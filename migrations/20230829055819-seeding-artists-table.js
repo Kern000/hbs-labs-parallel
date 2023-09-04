995bdb0920e6f39264eef6443aeff35688aacebf
Kern000 <holupkern@gmail.com>
2023-09-04 09:25:32 +0000

    recovered from gitpod wiping of database transiting to paid account

--- migrations/20230829055819-seeding-artists-table.js
+++ migrations/20230829055819-seeding-artists-table.js
@@ -19,7 +19,15 @@ exports.up = async function(db) {
   await db.insert('artists',['name'],['Cat Guy'])
   await db.insert('artists',['name'],['Weird Cat Guy'])
   await db.insert('artists',['name'],['Even Weirder Cat Guy'])
-  return db.insert('artists',['name'],['Justin Bobber'])
+  await db.insert('artists',['name'],['Justin Bobber'])
+  
+  await db.insert('media_properties',['name','description','url'],['bts', 'cats', 'btscats.com'])
+  await db.insert('media_properties',['name','description','url'],['dogs', 'woof', 'woof.com'])
+
+  await db.insert('posters',['title','cost','description', 'date', 'stock', 'height', 'width', 'thickness'],['meow1', '100', 'cats', '2022-01-01', '100', '20', '20', '10'])
+  await db.insert('posters',['title','cost','description', 'date', 'stock', 'height', 'width', 'thickness'],['meow2', '100', 'cats', '2022-01-01', '100', '20', '20', '10'])
+  return db.insert('posters',['title','cost','description', 'date', 'stock', 'height', 'width', 'thickness'],['meow3', '100', 'cats', '2022-01-01', '100', '20', '20', '10'])
+
 };
 
 exports.down = async function(db) {
